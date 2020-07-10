2c41f0aa2b68a4ea8936be3286789dad158b3c05
pradyuman.zala <pradyuman.zala@kevision.in>
2020-07-10 12:31:36 +0530

    made testimonials responsive

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b59c6b8b0853a9b7aa0401cf953c852e.js"
+  "/precache-manifest.00d1b45120483bfb9279d490a0de3d7f.js"
 );
 
 self.addEventListener('message', (event) => {
